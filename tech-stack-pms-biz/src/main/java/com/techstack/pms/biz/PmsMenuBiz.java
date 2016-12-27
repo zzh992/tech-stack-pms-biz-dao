ea9028ba4d2f289a5dd644758f6ef43f8109ecb3
zhuozehan <378853430@qq.com>
2016-12-27 20:46:25 +0800

    add getAllMenu function

--- tech-stack-pms-biz/src/main/java/com/techstack/pms/biz/PmsMenuBiz.java
+++ tech-stack-pms-biz/src/main/java/com/techstack/pms/biz/PmsMenuBiz.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 
@@ -466,6 +467,12 @@ public class PmsMenuBiz {
 		} else {
 			return meunList.size();
 		}
+	}
+
+
+	public List<PmsMenuDTO> getAllMenu() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
